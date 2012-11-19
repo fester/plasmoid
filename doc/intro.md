2d0c7553bde4cb36db3f02be37d311443391837b
Tim Baga <tim.baga@gmail.com>
2012-11-19 22:54:33 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to plasmoid
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/plasmoid/core_test.clj
@@ -0,0 +1,7 @@
+(ns plasmoid.core-test
+  (:use clojure.test
+        plasmoid.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
