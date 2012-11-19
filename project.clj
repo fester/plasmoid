b3dd7ccbc536695a7b0bd7b5f193728da0cb6a3a
Tim Baga <tim.baga@gmail.com>
2012-11-20 00:07:55 +0200

    Basic working app ready

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject plasmoid "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Generate fractal plasma images in Clojure. Just a test app."
+  :main plasmoid.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
--- src/plasmoid/core.clj
+++ src/plasmoid/core.clj
@@ -1,6 +1,21 @@
-(ns plasmoid.core)
+(ns plasmoid.core
+  [:import (java.io File FileWriter BufferedWriter)
+   (java.awt Color)
+   (java.awt.image BufferedImage)
+   (javax.imageio ImageIO)])
 
-(defn foo
+(defn plasma [width height]
+  (let [bi (BufferedImage. width height BufferedImage/TYPE_INT_ARGB)
+        g (.createGraphics bi)]
+    (do 
+      (.setColor g Color/BLUE)
+      (.fillRect g 0 0 width height)
+      (ImageIO/write bi "png" (File. (str width "x" height ".png"))))))
+
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [width height]
+  (let [integer-width (. Integer parseInt width)
+        integer-height (. Integer parseInt height)]
+    (plasma integer-width integer-height)
+    ))
