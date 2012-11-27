60492a2bb469632d304f2191ed0d4c56713f2fc0
Tim Baga <tim.baga@gmail.com>
2012-11-27 23:20:02 +0200

    Seting arbitrary pixel works

--- src/plasmoid/core.clj
+++ src/plasmoid/core.clj
@@ -5,14 +5,24 @@
    (javax.imageio ImageIO)]
   [:require [plasmoid.color :refer :all] [plasmoid.tools :refer :all]])
 
+(defn set-pixel [image x y color]
+  (.setRGB image x y (int-color color)))
+  
+
 (defn plasma [width height]
   (let [bi (BufferedImage. width height BufferedImage/TYPE_INT_ARGB)
         g (.createGraphics bi)
-        color-a (random-color)
-        color-b (random-color)
-        surface-color (average-color color-a color-b)]
-    (.setColor g (awt-color surface-color))
-    (.fillRect g 0 0 width height)
+        x1 0
+        y1 0
+        x2 (dec width)
+        y2 (dec height)]
+    (.setColor g Color/BLACK)
+    (.fillRect g 0  0 width height)
+
+    (set-pixel bi x1 y1 (random-color))
+    (set-pixel bi x2 y1 (random-color))
+    (set-pixel bi x1 y2 (random-color))
+    (set-pixel bi x2 y2 (random-color))
     (ImageIO/write bi "png" (File. (str width "x" height ".png")))))
 
 (defn -main
